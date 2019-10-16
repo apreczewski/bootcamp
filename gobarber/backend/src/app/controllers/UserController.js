df91bcd69cdc7cd38806aab4fcc9db775d342366
Alexandre Preczewski <apreczewski@gmail.com>
2019-10-16 20:14:31 -0300

    gobaber: backend and frontend

---
+++ gobarber/backend/src/app/controllers/UserController.js
@@ -0,0 +1,96 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+import File from '../models/File';
+
+class UserController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails' });
+    }
+
+    const userExists = await User.findOne({
+      where: { email: request.body.email },
+    });
+
+    if (userExists) {
+      return response
+        .status(400)
+        .json({ error: 'Email already exists on database!' });
+    }
+
+    const { id, name, email, provider } = await User.create(request.body);
+    return response.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  async update(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email, oldPassword } = request.body;
+
+    const user = await User.findByPk(request.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return response.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return response.status(401).json({ error: 'Password does not mach' });
+    }
+
+    await user.update(request.body);
+
+    const { id, name, avatar } = await User.findByPk(request.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+
+    return response.json({
+      id,
+      name,
+      email,
+      avatar,
+    });
+  }
+}
+
+export default new UserController();
---
+++ gobarber/frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from '~/routes';
+
+import GlobalStyle from '~/styles/global';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes />
+      <GlobalStyle />
+      <ToastContainer autoClose={3000} />
+    </BrowserRouter>
+  );
+}
+
+export default App;
